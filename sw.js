824a86594ddb0321d458fffde94eee0ab20e5404
YiM2onZhoi <1269583034@qq.com>
2024-12-06 12:06:25 +0800

    Site updated: 2024-12-06 12:06:25

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1733457892384";
+const VERSION = "1733457983040";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
